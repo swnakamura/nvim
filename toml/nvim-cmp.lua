e23d82ac1b758407479cad5b093a96f20b722b87
woodyZootopia <piequalsabout314159@gmail.com>
2022-10-19 11:04:33 +0900

    Enable luasnip completion

--- toml/nvim-cmp.lua
+++ toml/nvim-cmp.lua
@@ -6,9 +6,9 @@ cmp.setup({
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
             -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
             -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
         end,
     },
     window = {
@@ -29,7 +29,7 @@ cmp.setup({
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'ultisnips' }, -- For ultisnips users.
+        { name = 'luasnip' },
     }, {
         { name = 'buffer' },
         { name = 'path' },
