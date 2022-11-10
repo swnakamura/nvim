f7af662873980873aa220160d176fbfdba5c319d
woodyZootopia <piequalsabout314159@gmail.com>
2022-11-10 09:27:18 +0900

    Treesitter update

--- toml/treesitter.lua
+++ toml/treesitter.lua
@@ -2,12 +2,12 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = {'diff', 'org'},
-    disable = {'json', 'diff', 'org'},
+    disable = {'json', 'diff'},
   },
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = "vac",
+      init_selection = "M",
       node_incremental = "M",
       scope_incremental = "S",
       node_decremental = "m",
