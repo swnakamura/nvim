44c63038550f551655b57e8721821f0edda0115e
woodyZootopia <piequalsabout314159@gmail.com>
2023-02-09 22:50:29 +0900

    Use nvim-cmp lsp in tex and bashls

--- toml/nvim-cmp_and_lsp.lua
+++ toml/nvim-cmp_and_lsp.lua
@@ -129,7 +129,7 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', 'gR', vim.lsp.buf.references, bufopts)
     vim.keymap.set('n', 'gF', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('lspconfig')['pyright'].setup {
     capabilities = capabilities,
     on_attach = on_attach,
