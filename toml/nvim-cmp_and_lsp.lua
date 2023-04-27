7b64ea5c258e58efc101d6b4128c7f6048ad36f8
woodyZootopia <piequalsabout314159@gmail.com>
2023-04-27 22:34:30 +0900

    Different setting for lua for neovim settings

--- toml/nvim-cmp_and_lsp.lua
+++ toml/nvim-cmp_and_lsp.lua
@@ -131,7 +131,7 @@ local on_attach = function(client, bufnr)
 end
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local servers = { 'pyright', 'rust_analyzer', 'lua_ls', 'bashls', 'clangd', 'texlab', 'vimls' }
+local servers = { 'pyright', 'rust_analyzer', 'bashls', 'clangd', 'texlab', 'vimls' }
 
 for _, lsp in ipairs(servers) do
     require'lspconfig'[lsp].setup {
@@ -139,3 +139,32 @@ for _, lsp in ipairs(servers) do
         capabilities = capabilities
     }
 end
+
+require('lspconfig')['lua_ls'].setup {
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {
+                    'vim',
+                    'require'
+                },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
