062c573410cbb236f19a96e93ca41b40505bed90
woodyZootopia <piequalsabout314159@gmail.com>
2023-06-29 15:10:09 +0900

    Undo gr to change name

--- init.lua
+++ init.lua
@@ -154,11 +154,11 @@ hi link agitDiffRemove diffRemoved
               vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
             end
 
-            nmap('gr', vim.lsp.buf.rename, '[R]e[n]ame')
+            nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
             nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
             nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-            -- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+            nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
             nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
             nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
             -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
