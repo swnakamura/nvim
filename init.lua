d03a5117b075da9ffcb25d966a07091d12eec8c7
woodyZootopia <piequalsabout314159@gmail.com>
2023-07-03 23:12:17 +0900

    Install treesitter markdown and tex syntax

--- init.lua
+++ init.lua
@@ -861,7 +861,7 @@ hi CursorWord guibg=#282d44
     config = function()
       require('nvim-treesitter.configs').setup {
         -- Add languages to be installed here that you want installed for treesitter
-        ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+        ensure_installed = { 'c', 'cpp', 'go', 'lua', 'markdown', 'python', 'rust', 'tex', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
         -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
         auto_install = false,
