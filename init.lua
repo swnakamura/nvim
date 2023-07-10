05d6a71b9e147cae4077838cbf8eedfed9b0e365
woodyZootopia <piequalsabout314159@gmail.com>
2023-07-10 19:27:43 +0900

    Update lualine settings

--- init.lua
+++ init.lua
@@ -847,7 +847,52 @@ hi CursorWord guibg=#282d44
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = true
+    config = function()
+      local custom_color = require('lualine.themes.iceberg_dark')
+      custom_color.normal.c.fg = '#6b7089'
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = custom_color,
+          -- component_separators = { left = '', right = '' },
+          -- section_separators = { left = '', right = '' },
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+          }
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'encoding', 'fileformat', 'filetype', 'progress', 'location', 'filename' },
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename', 'location' },
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {}
+      }
+    end
   },
 
   {
