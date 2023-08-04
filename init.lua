fef8a1f9f6415974bceae480bf5dc125e941b382
woodyZootopia <piequalsabout314159@gmail.com>
2023-08-04 10:42:13 +0900

    <leader>q to close man window

--- init.lua
+++ init.lua
@@ -1552,7 +1552,7 @@ vim.keymap.set('n', '<leader>wq', '<Cmd>quitall<CR>')
 -- On certain files, quit by <leader>q
 vim.api.nvim_create_augroup('bdel-quit', {})
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'gitcommit', 'lazy', 'help' },
+  pattern = { 'gitcommit', 'lazy', 'help', 'man' },
   callback = function()
     vim.keymap.set('n', '<leader>q', '<Cmd>q<CR>')
   end,
