81c3c752cf611d7fe1d92ee77b112962b628ef65
swnakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-01-31 10:37:13 +0900

    feat: add remote-nvim

--- init.lua
+++ init.lua
@@ -272,6 +272,28 @@ require('lazy').setup({
     end
   },
 
+  {
+    "amitds1997/remote-nvim.nvim",
+    version = "*", -- Pin to GitHub releases
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- For standard functions
+      "MunifTanjim/nui.nvim", -- To build the plugin UI
+      "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+    },
+    opts = {
+      remote = {
+        copy_dirs = {
+          config = {
+            compression = {
+              enabled = true,
+              additional_opts = { "--exclude-vcs" },
+            },
+          },
+        },
+      },
+    }
+  },
+
   -- Git related plugins
   {
     "NeogitOrg/neogit",
