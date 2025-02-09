bc99b0616643f86924e5f4c19f153bc0fa9ac649
swnakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-02-09 16:06:54 +0900

    refactor: move gitsigns.nvim to git section

--- init.lua
+++ init.lua
@@ -435,6 +435,80 @@ require('lazy').setup({
       ]])
     end
   },
+  {
+    -- Adds git releated signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    event = 'VeryLazy',
+    opts = {
+      signs_staged_enable = false,
+      signcolumn = false,
+      numhl      = true,
+      on_attach  = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        -- pagedown, ]h, <Down> does the same thing
+        map('n', '<PageDown>', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk({ preview = true }) end)
+          return '<Ignore>'
+        end, { expr = true })
+        map('n', ']h', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk({ preview = true }) end)
+          return '<Ignore>'
+        end, { expr = true })
+        map('n', '<Down>', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk({ preview = true }) end)
+          return '<Ignore>'
+        end, { expr = true })
+
+        -- pageup, [h, <Up> does the same thing
+        map('n', '<PageUp>', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk({ preview = true }) end)
+          return '<Ignore>'
+        end, { expr = true })
+        map('n', '[h', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk({ preview = true }) end)
+          return '<Ignore>'
+        end, { expr = true })
+        map('n', '<Up>', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk({ preview = true }) end)
+          return '<Ignore>'
+        end, { expr = true })
+
+        -- Actions
+        map('n', '<leader>hs', gs.stage_hunk)
+        map('n', '<C-Up>', gs.stage_hunk)
+        map('n', '<Right>', gs.stage_hunk)
+        map('n', '<leader>hu', gs.reset_hunk)
+        map('v', '<leader>hs', function() gs.stage_hunk { fn.line("."), fn.line("v") } end)
+        map('v', '<leader>hu', function() gs.reset_hunk { fn.line("."), fn.line("v") } end)
+        map('n', '<leader>hS', gs.stage_buffer)
+        map('n', '<leader>hr', gs.undo_stage_hunk)
+        map('n', '<leader>hR', gs.reset_buffer)
+        map('n', '<leader>hp', gs.preview_hunk)
+        map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+        -- map('n', '<leader>tb', gs.toggle_current_line_blame)
+        map('n', '<leader>hd', gs.diffthis)
+        map('n', '<leader>hD', function() gs.diffthis('~') end)
+        -- map('n', '<leader>td', gs.toggle_deleted)
+
+        -- Text object
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+      end
+    },
+  },
 
   -- dmacro for recording automatically
   {
@@ -1831,80 +1905,6 @@ $0
 
   -- Useful plugin to show you pending keybinds.
   -- { 'folke/which-key.nvim',                                opts = {}, event = 'BufEnter' },
-  {
-    -- Adds git releated signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    event = 'VeryLazy',
-    opts = {
-      signs_staged_enable = false,
-      signcolumn = false,
-      numhl      = true,
-      on_attach  = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Navigation
-        -- pagedown, ]h, <Down> does the same thing
-        map('n', '<PageDown>', function()
-          if vim.wo.diff then return ']c' end
-          vim.schedule(function() gs.next_hunk({ preview = true }) end)
-          return '<Ignore>'
-        end, { expr = true })
-        map('n', ']h', function()
-          if vim.wo.diff then return ']c' end
-          vim.schedule(function() gs.next_hunk({ preview = true }) end)
-          return '<Ignore>'
-        end, { expr = true })
-        map('n', '<Down>', function()
-          if vim.wo.diff then return ']c' end
-          vim.schedule(function() gs.next_hunk({ preview = true }) end)
-          return '<Ignore>'
-        end, { expr = true })
-
-        -- pageup, [h, <Up> does the same thing
-        map('n', '<PageUp>', function()
-          if vim.wo.diff then return '[c' end
-          vim.schedule(function() gs.prev_hunk({ preview = true }) end)
-          return '<Ignore>'
-        end, { expr = true })
-        map('n', '[h', function()
-          if vim.wo.diff then return '[c' end
-          vim.schedule(function() gs.prev_hunk({ preview = true }) end)
-          return '<Ignore>'
-        end, { expr = true })
-        map('n', '<Up>', function()
-          if vim.wo.diff then return '[c' end
-          vim.schedule(function() gs.prev_hunk({ preview = true }) end)
-          return '<Ignore>'
-        end, { expr = true })
-
-        -- Actions
-        map('n', '<leader>hs', gs.stage_hunk)
-        map('n', '<C-Up>', gs.stage_hunk)
-        map('n', '<Right>', gs.stage_hunk)
-        map('n', '<leader>hu', gs.reset_hunk)
-        map('v', '<leader>hs', function() gs.stage_hunk { fn.line("."), fn.line("v") } end)
-        map('v', '<leader>hu', function() gs.reset_hunk { fn.line("."), fn.line("v") } end)
-        map('n', '<leader>hS', gs.stage_buffer)
-        map('n', '<leader>hr', gs.undo_stage_hunk)
-        map('n', '<leader>hR', gs.reset_buffer)
-        map('n', '<leader>hp', gs.preview_hunk)
-        map('n', '<leader>hb', function() gs.blame_line { full = true } end)
-        -- map('n', '<leader>tb', gs.toggle_current_line_blame)
-        map('n', '<leader>hd', gs.diffthis)
-        map('n', '<leader>hD', function() gs.diffthis('~') end)
-        -- map('n', '<leader>td', gs.toggle_deleted)
-
-        -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-      end
-    },
-  },
 
   -- colorscheme
   {
