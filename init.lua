3be68defce03b759506b19873ceb1e1a4c573829
swnakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-04-12 00:08:36 +0900

    feat: add budouxify.nvim (disabled)

--- init.lua
+++ init.lua
@@ -2377,6 +2377,30 @@ $0
   },
 
   {
+    cond=false,
+    "https://github.com/atusy/budouxify.nvim",
+    dependencies = {"https://github.com/atusy/budoux.lua"},
+    config = function()
+      vim.keymap.set("n", "W", function()
+          local pos = require("budouxify.motion").find_forward({
+              head = true,
+          })
+          if pos then
+              vim.api.nvim_win_set_cursor(0, { pos.row, pos.col })
+          end
+      end)
+      vim.keymap.set("n", "E", function()
+          local pos = require("budouxify.motion").find_forward({
+              head = false,
+          })
+          if pos then
+              vim.api.nvim_win_set_cursor(0, { pos.row, pos.col })
+          end
+      end)
+    end
+  },
+
+  {
     cond = vim.g.is_macos and fn.isdirectory(fn.expand('~/ghq/github.com/swnakamura/novel_formatter')) == 1,
     dir = '~/ghq/github.com/swnakamura/novel_formatter'
   },
