29f76df3cc605300931f05f567a1a564fd642d5b
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-05-10 01:00:50 +0900

    Add avante.nvim

--- init.lua
+++ init.lua
@@ -740,6 +740,72 @@ require('lazy').setup({
     end,
     -- See Commands section for default commands if you want to lazy load on them
   },
+  {
+  "yetone/avante.nvim",
+  version = false, -- Never set this value to "*"! Never!
+  keys = {
+      '<leader>aa',
+      '<leader>ad',
+      '<leader>af',
+      '<leader>ah',
+      '<leader>an',
+      '<leader>ar',
+      '<leader>aR',
+      '<leader>as',
+      '<leader>aS',
+      '<leader>at',
+      '<leader>a?',
+  },
+  opts = {
+    -- add any opts here
+    -- for example
+    provider = "copilot",
+    copilot = {
+      model = "gpt-4.1",
+    }
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+},
 
   -- register preview
   {
@@ -2048,7 +2114,7 @@ $0
         autojump = true,
       })
       -- You probably also want to set a keymap to toggle aerial
-      vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>")
+      vim.keymap.set("n", "<leader>ta", "<cmd>AerialToggle<CR>")
     end
   },
 
