995a904e08ea2b650f04e5e2e57a1678fb2170f7
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-05-13 00:00:58 +0900

    refactor(init): remove img-clip and render-markdown plugins
    - Removed 'HakonHarnes/img-clip.nvim' and 'MeanderingProgrammer/render-markdown.nvim'
      from the plugin list to simplify configuration.
    - Updated Floaterm key mapping to be buffer-local for 'floaterm' filetype,
      preventing global mapping conflicts.

--- init.lua
+++ init.lua
@@ -549,7 +549,7 @@ require('lazy').setup({
       vapi.nvim_create_autocmd('FileType', {
         pattern = 'floaterm',
         callback = function()
-          map('n', [[<C-;>]], [[<C-\><C-n>:FloatermHide<CR>]], { silent = true })
+          map('n', [[<C-;>]], [[<C-\><C-n>:FloatermHide<CR>]], { silent = true, buffer = 0 })
         end
       })
       map('t', [[<C-/>]], [[<C-\><C-n>:FloatermHide<CR>]], { silent = true })
@@ -785,33 +785,8 @@ require('lazy').setup({
     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
+    }
   },
-},
 
   -- register preview
   {
