ade6de6c653771fcb43fcc9c1607f7d052641d71
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-05-14 20:36:40 +0900

    feat(lsp): add tiny-inline-diagnostic for better UX
    Added the 'rachartier/tiny-inline-diagnostic.nvim' plugin to improve
    inline LSP diagnostics display. Configured to enable multiline
    diagnostics and show all diagnostics on the cursor line. Disabled
    native virtual_text diagnostics to prevent overlap.

--- init.lua
+++ init.lua
@@ -305,6 +305,24 @@ require('lazy').setup({
     end
   },
 
+  -- prettier diagnostics
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy", -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+        require('tiny-inline-diagnostic').setup({
+        options = {
+          multilines = {
+            enabled = true,
+            always_show = false,
+          },
+          show_all_diags_on_cursorline = true,
+        }
+      })
+        vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+    end
+  },
   -- lazydev
   {
     "folke/lazydev.nvim",
