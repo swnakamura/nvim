f9c87e4066c09d32ff56c329308a96510a9c967a
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-06-02 20:17:59 +0900

    feat(japanese): modularize and extend IME integration
    Refactor Japanese IME support by moving platform-specific commands
    to a new `japanese/common.lua` module. Rename and update
    `japanese_keep.lua` to `japanese/keep.lua`, using the shared
    commands and renaming `save_status` to `stash_status`. Add a new
    `japanese/mode.lua` module for enabling, disabling, and toggling
    IME, with related autocommands and buffer-local keymaps for
    Japanese text navigation. Update main config to use the new
    modules and add an <F2> keymap for toggling IME mode.

---
+++ lua/japanese/mode.lua
@@ -0,0 +1,73 @@
+local M = require('japanese.common')
+
+-- Functions to enable/disable IME
+
+M.enable = function()
+  os.execute(M.commands[M.osname].on)
+end
+
+M.disable = function()
+  os.execute(M.commands[M.osname].off)
+end
+
+M.toggle_IME = function()
+  vim.b.IME_autoenable = not vim.b.IME_autoenable
+  if vim.b.IME_autoenable then
+    print('日本語入力モードON')
+    if vim.fn.mode() == 'i' then
+      M.enable()
+    end
+    -- also set keymap for FuzzyMotion, which is useful for Japanese text
+    vim.api.nvim_buf_set_keymap(0, 'n', 'S',     '<cmd>FuzzyMotion<CR>', { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(0, 'n', '<C-s>', '<cmd>FuzzyMotion<CR>', { noremap = true, silent = true })
+  else
+    print('日本語入力モードOFF')
+    vim.api.nvim_buf_del_keymap(0, 'n', 'S')
+    if vim.fn.mode() == 'i' then
+      M.disable()
+    end
+  end
+end
+
+-- Autocommands that are controlled by the above functions
+vim.api.nvim_create_augroup("IME_autotoggle", { clear = true })
+vim.api.nvim_create_autocmd("InsertEnter", {
+  group = "IME_autotoggle",
+  pattern = "*",
+  callback = function()
+    if vim.b.IME_autoenable then
+      M.enable()
+    end
+  end,
+})
+vim.api.nvim_create_autocmd("InsertLeave", {
+  group = "IME_autotoggle",
+  pattern = "*",
+  callback = function()
+    M.disable()
+  end,
+})
+vim.api.nvim_create_autocmd("CmdLineEnter", {
+  group = "IME_autotoggle",
+  pattern = [[/,\?]],
+  callback = function()
+    if vim.b.IME_autoenable then
+      vim.keymap.set('c', '<CR>', '<Plug>(kensaku-search-replace)<CR>')
+    else
+      -- set and delete to return to the default behavior
+      vim.keymap.set('c', '<CR>', '<CR>')
+      vim.keymap.del('c', '<CR>')
+    end
+  end,
+})
+
+vim.api.nvim_create_augroup("auto_ja", { clear = true })
+vim.api.nvim_create_autocmd("BufRead", {
+  group = "auto_ja",
+  pattern = { "*/my-text/**.txt", "*/my-text/**.md", "*/obsidian/**.md" },
+  callback = function()
+    M.toggle_IME()
+  end,
+})
+
+return M
