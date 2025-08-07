dcf4c8c033f9331f748ad1e0a38b618bef91b131
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-08-07 23:03:06 +0900

    feat(cmp): add blink-cmp-spell for spell suggestions
    Added 'ribru17/blink-cmp-spell' as a dependency and configured it as
    a new completion source for nvim-cmp. The spell source is enabled for
    markdown and other filetypes, but only activates in Treesitter @spell
    captures and is disabled in @nospell captures. This enhances spelling
    suggestions contextually during completion, especially in markdown
    files.

--- init.lua
+++ init.lua
@@ -1197,7 +1197,8 @@ require('lazy').setup({
 
     dependencies = {
       { "epwalsh/obsidian.nvim" },
-      { 'L3MON4D3/LuaSnip' }
+      { 'L3MON4D3/LuaSnip' },
+      { 'ribru17/blink-cmp-spell' },
     },
 
     -- use a release tag to download pre-built binaries
@@ -1282,9 +1283,9 @@ require('lazy').setup({
       sources = {
         default = function()
           if vim.bo.filetype == 'markdown' then
-            return { 'obsidian', 'obsidian_new', 'obsidian_tags', 'lazydev', 'lsp', 'path', 'snippets', 'buffer' }
+            return { 'obsidian', 'spell', 'obsidian_new', 'obsidian_tags', 'lazydev', 'lsp', 'path', 'snippets', 'buffer' }
           else
-            return { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' }
+            return { 'lazydev', 'lsp', 'spell', 'path', 'snippets', 'buffer' }
           end
         end,
         providers = {
@@ -1309,6 +1310,31 @@ require('lazy').setup({
             module = "lazydev.integrations.blink",
             -- make lazydev completions top priority (see `:h blink.cmp`)
             score_offset = 100,
+          },
+          spell = {
+            name = 'Spell',
+            module = 'blink-cmp-spell',
+            opts = {
+              -- EXAMPLE: Only enable source in `@spell` captures, and disable it
+              -- in `@nospell` captures.
+              enable_in_context = function()
+                local curpos = vim.api.nvim_win_get_cursor(0)
+                local captures = vim.treesitter.get_captures_at_pos(
+                  0,
+                  curpos[1] - 1,
+                  curpos[2] - 1
+                )
+                local in_spell_capture = false
+                for _, cap in ipairs(captures) do
+                  if cap.capture == 'spell' then
+                    in_spell_capture = true
+                  elseif cap.capture == 'nospell' then
+                    return false
+                  end
+                end
+                return in_spell_capture
+              end,
+            },
           },
         },
       },
