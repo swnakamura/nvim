60ffdc7d38d848e5131d5674e084fa035eae5e0c
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-09-02 10:30:25 +0900

    feat(markdown): improve readability with linebreak
    Added 'setlocal linebreak' to the Markdown filetype plugin. This ensures
    that wrapped lines break at word boundaries, making long lines easier to
    read in the editor.

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,5 +1,6 @@
 vim.cmd([[
   setlocal conceallevel=1
+  setlocal linebreak
   command -range=% MDPTCopy lua require('markdown').obsidian_to_plain_text_copy(<line1>, <line2>)
   command -range=% MDGHCopy lua require('markdown').obsidian_to_github_copy(<line1>, <line2>)
 ]])
