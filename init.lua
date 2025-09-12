0c894e0c90028a487e5265563d5b2e23a3fe7b66
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-09-12 16:58:30 +0900

    feat(todo-comments): add keymaps for navigation
    Added custom config for folke/todo-comments.nvim to set up keymaps: ]t
    for next todo comment and [t for previous. This enhances navigation
        between todo comments in normal mode.

--- init.lua
+++ init.lua
@@ -312,7 +312,12 @@ require('lazy').setup({
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {}
+    opts = {},
+    config = function()
+      require("todo-comments").setup {}
+      vim.keymap.set("n", "]t", function() require("todo-comments").jump_next() end, { desc = "Next todo comment" })
+      vim.keymap.set("n", "[t", function() require("todo-comments").jump_prev() end, { desc = "Previous todo comment" })
+    end
   },
 
   -- prettier diagnostics
