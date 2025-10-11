13f50f864578d07cddc560d6c191e8a4fb4b902b
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-10-11 16:05:59 -0700

    feat(ui): enable scroll except in Neovide/VSCode
    The scroll feature is now enabled by default, except when running in
    Neovide or VSCode environments. This change improves usability by
    allowing scroll functionality in supported environments while
    maintaining compatibility with Neovide and VSCode where scrolling may
    not be desired.

--- init.lua
+++ init.lua
@@ -301,8 +301,7 @@ require('lazy').setup({
         picker = { enabled = true, },
         scope = { enabled = true },
         scroll = {
-          -- enabled = not vim.g.neovide and not vim.g.is_vscode,
-          enabled = false,
+          enabled = not vim.g.neovide and not vim.g.is_vscode,
           filter = function(buf)
             return vim.bo[buf].buftype ~= "terminal" and vim.bo[buf].filetype ~= "copilot-chat"
           end
