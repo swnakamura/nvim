17129aac577bbdc7d33906afe9547af548c4c25b
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-10-22 18:17:25 -0700

    feat(todo-comments): move keymaps to lazy keys field
    Refactored todo-comments.nvim plugin setup by relocating the keymap
    definitions for jumping to next and previous todo comments from the
    config function to the 'keys' field in lazy.nvim. This enables
    declarative keymap registration and removes manual keymap setup,
    improving maintainability and consistency.

--- init.lua
+++ init.lua
@@ -320,10 +320,12 @@ require('lazy').setup({
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+    },
     config = function()
       require("todo-comments").setup {}
-      vim.keymap.set("n", "]t", function() require("todo-comments").jump_next() end, { desc = "Next todo comment" })
-      vim.keymap.set("n", "[t", function() require("todo-comments").jump_prev() end, { desc = "Previous todo comment" })
     end
   },
 
