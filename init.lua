a2535a412c2a41b438b7ba9e78757bee1f9b65ef
Shu Nakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2025-10-22 19:00:00 -0700

    feat(markdown): add markdown-preview.nvim plugin
    Added iamcco/markdown-preview.nvim to enable live Markdown preview in
    Neovim. Configured to load for Markdown files only, with commands for
    preview control and a build step for setup.

--- init.lua
+++ init.lua
@@ -2127,6 +2127,15 @@ require('lazy').setup({
       })
     end
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 
   -- vim table mode for markdown
   {
